3b2e5a85a071bbfd26ab859e2f802944615f8d18
MarekMoric <xmoric@mendelu.cz>
2025-06-30 14:57:41 +0200

    POST implemented

--- src/main/java/com/marek/cashcard/CashCardController.java
+++ src/main/java/com/marek/cashcard/CashCardController.java
@@ -1,11 +1,10 @@
 package com.marek.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController /*makes class a Component of type RestController capable of handling http req*/
@@ -26,5 +25,15 @@ class CashCardController {
             return ResponseEntity.notFound().build(); /*404 response*/
         }
 
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest); /*id is created here by database*/
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}") /*construction of URI*/
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
